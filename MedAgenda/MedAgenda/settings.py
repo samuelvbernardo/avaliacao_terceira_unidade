f51e400a1126327a45fa4fb1460d010be18d075e
Samuel Bernardo <samuelbenardo@ads.fiponline.edu.br>
2025-06-05 14:52:23 -0300

    chore: initial setup for database migration

--- MedAgenda/MedAgenda/settings.py
+++ MedAgenda/MedAgenda/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "MedAgenda.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'med_agenda',       
+        'USER': 'postgres',
+        'PASSWORD': '7458',
+        'HOST': 'localhost',
+        'PORT': '5432',                     
     }
 }
 
